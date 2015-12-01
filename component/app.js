4f1627c31e0622ac063c2ae413cc9b6b1fa72d0d
Felipe Apostol <flipjs.io@gmail.com>
2015-12-01 00:10:48 +0000

    initial commit

---
+++ component/app.js
@@ -0,0 +1,26 @@
+Vue.component('alert', {
+  template: '#alert-template',
+  props: ['type'],
+  data: function () {
+    return {
+      show: true
+    }
+  },
+  computed: {
+    alertClasses: function () {
+      var type = this.type
+      return {
+        'alert': true,
+        'alert--success': type === 'success',
+        'alert--error': type === 'error'
+      }
+    }
+  }
+})
+
+new Vue({
+  el: 'body',
+  data: {
+    test: 'hello world'
+  }
+})
